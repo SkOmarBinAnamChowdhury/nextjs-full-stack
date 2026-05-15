dffac8f1c08a196fab2bd2be0f91fb27737889fc
Sk Omar Bin Anam Chowdhury <skomarbinanam@gmail.com>
2026-05-15 23:53:19 +0600

    add IUser interface to User model

--- models/User.ts
+++ models/User.ts
@@ -1,2 +1,12 @@
 import mongoose, {Schema, model, models} from "mongoose";
 import bcrypt from "bcryptjs";
+
+export interface IUser {
+    email: string;
+    password: string;
+    _id?: mongoose.Types.ObjectId;
+    createdAt?: Date;
+    updatedAt?: Date;
+
+}
+
