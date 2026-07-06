98463478958994a4386e0d0484c36826e48060e5
Sk Omar Bin Anam Chowdhury <skomarbinanam@gmail.com>
2026-07-06 15:00:43 +0600

    add Video model with schema and interface definition

--- models/User.ts
+++ models/User.ts
@@ -27,6 +27,6 @@ userSchema.pre("save", async function (next) {
 
 
 
-const User = models?.User || model<IUser>("User", userSchema)
+const User = models?.User || model<IUser>("User", userSchema);
 
-export default User
+export default User;
