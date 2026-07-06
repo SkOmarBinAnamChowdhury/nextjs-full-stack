ef70282f0809a634ca04c499246d638f13d3b6ee
Sk Omar Bin Anam Chowdhury <skomarbinanam@gmail.com>
2026-07-06 15:44:11 +0600

    refactor User model to remove unnecessary next() call in pre-save hook

--- models/User.ts
+++ models/User.ts
@@ -18,11 +18,10 @@ const userSchema = new Schema<IUser>(
     timestamps: true,
   },
 );
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 10);
   }
-  next();
 });
 
 
